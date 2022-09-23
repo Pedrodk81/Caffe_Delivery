ffa3e91454adaa6be6c93f9f78e1e875b303e438
Pedro Duke <pedrodk81@gmail.com>
2022-09-22 22:19:10 -0300

    Update Order.ts

--- src/controllers/Order.ts
+++ src/controllers/Order.ts
@@ -1,44 +1,65 @@
 import { Express, Response, Request } from "express";
 import { prisma } from "../database/prismaClient";
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from "@prisma/client";
+
 interface ICreateOrder {
   user_id: number;
-  coffee_id: number;
+  order_coffee_id: number;
   total: number;
 }
 
 export default class OrderController {
-  public static async createOrder(
-    req: Request,
-    res: Response
-  ) {
+  public static async createOrder(req: Request, res: Response) {
     const { id } = req.user;
-    const { coffee_id, incremenet } = req.params;
+    const { order_coffee_id, incremenet, user_id } = req.params;
 
-    let order: Prisma.OrderCreateInput
-    // order = await prisma.order.findFirst({ 
-    //     where: {
-    //       user_id: id,
-    //       payment_at: null
-    //     } 
-    //   });
-
-    let user = await prisma.user.findFirst({
+    let user = await prisma.order.findFirst({
       where: {
-        id
-      }
-    })
-    
-    if(!order) {
-      order = await prisma.order.create({ 
+        user_id: {
+          equals: id,
+        },
+      },
+    });
+    if (!user) {
+      const order = await prisma.order.create({
         data: {
-          user_id: 1,
-          Order_coffee: {
-            coffee_id,
-          }
-        } 
+          user_id: Number(user_id),
+          order_coffee_id: Number(order_coffee_id),
+        },
       });
+
+      res.status(200).send(order);
     }
+
+    // let order: Prisma.OrderCreateInput
+    // // order = await prisma.order.findFirst({
+    // //     where: {
+    // //       user_id: id,
+    // //       payment_at: null
+    // //     }
+    // //   });
+
+    // let user = await prisma.user.findFirst({
+    //   where: {
+    //     id:id
+    //   }
+    // })
+
+    // if(!order) {
+    //   order = await prisma.order.create({
+    //     data:{
+
+    //     }
+    //   })
+    // order = await prisma.order.create({
+    //   data: {
+    //     user_id: 1,
+    //     Order_coffee: {
+    //       coffee_id,
+    //     }
+    //   }
+    // });
+    // }
 
     // const order = await prisma.order.create({
     //   data: {
@@ -46,7 +67,5 @@ export default class OrderController {
     //     total,
     //   },
     // });
-
-    res.status(200).send(order);
   }
 }
